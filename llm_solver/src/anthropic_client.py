62448f11b1868ded41aa85565fe0e8393f358bbc
galt <galtfilin@gmail.com>
2024-09-05 15:19:53 +0300

    Validate resp schema if schema is provided

--- llm_solver/src/anthropic_client.py
+++ llm_solver/src/anthropic_client.py
@@ -5,6 +5,7 @@ import typing
 from dataclasses import dataclass
 
 import anthropic
+import jsonschema
 from diskcache import Cache
 
 from src import consts
@@ -26,27 +27,28 @@ class SchemaValidationError(AssertionError):
 
 
 class AnthropicWrapper:
-    def __init__(self):
+    def __init__(self, system_prompt: str):
         api_key = os.environ.get("LEETCODE_SOLVER_CLAUDE_API_KEY")
         if not api_key:
             raise RuntimeError("API key env var was not set")
         self._client = anthropic.Anthropic(api_key=api_key, max_retries=5)
+        self.system_prompt = system_prompt
 
-    def send_prompt(self, system_prompt: str, contents: typing.List[ChatMessage]) -> ChatMessage:
+    def send_prompt(self, contents: typing.List[ChatMessage]) -> ChatMessage:
         if len(contents) == 0:
             raise ValueError("Received empty prompt contents")
         messages = [msg.to_dict() for msg in contents]
-        cache_key = (system_prompt, contents)
+        cache_key = (self.system_prompt, contents)
         cached_res = cache.get(cache_key)
         if cached_res:
-            logging.debug("cache hit: %s", system_prompt)
+            logging.debug("cache hit: %s", self.system_prompt)
             return cached_res
         logging.info("Cache miss")
-        logging.debug("Cache miss for: %s(%d)", system_prompt, len(messages))
+        logging.debug("Cache miss for: %s(%d)", self.system_prompt, len(messages))
         resp = self._client.messages.create(
             max_tokens=4096,
             model="claude-3-5-sonnet-20240620",
-            system=system_prompt,
+            system=self.system_prompt,
             temperature=0,
             messages=messages,
         )
@@ -63,7 +65,8 @@ class AnthropicWrapper:
         return wrapped_resp
 
 
-def extract_json_from_prompt_text_block(text_block: str):
+def extract_json_from_prompt_text_block(text_block: str, expected_schema=None) -> dict:
+    logging.debug("extracting json from: %s", text_block)
     res = {}
     if "```json" in text_block:
         start = text_block.index("```json")
@@ -76,4 +79,11 @@ def extract_json_from_prompt_text_block(text_block: str):
         res = json.loads(text_block)
     except json.JSONDecodeError as e:
         logging.error("could not decode json from string with error: %s", e)
+    logging.debug("Extracted json: %s", res)
+    if expected_schema:
+        try:
+            jsonschema.validate(res, expected_schema)
+        except jsonschema.ValidationError as vex:
+            logging.error("Generated res has an unexpected schema: %s", vex, exc_info=True)
+            raise SchemaValidationError(f"Generated res has an unexpected schema: {vex}")
     return res
