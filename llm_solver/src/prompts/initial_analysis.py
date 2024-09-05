ba4b980601af1f377394c79c31a6510ca320a50d
galt <galtfilin@gmail.com>
2024-09-05 11:11:43 +0300

    Separate sys prompt from analysis prompt

---
+++ llm_solver/src/prompts/initial_analysis.py
@@ -0,0 +1,5 @@
+PROMPT="""Analyze the following technical question, explain the given examples, and suggest the best possible solution's run and memory complexity.
+Return the response as a minified JSON in the following format:
+{"explanation":"in this question we are asked to ...","examples":[{"Example 1":"In this example we get X, apply the operations Y1, Y2, Y3 and that's how we get the output Z"},{"Example 2":"These are the steps that are applied to the inputs to get the output: ..."}],"best_achievable_runtime":"O(N)","best_achievable_memory":"O(1)"}
+
+The Question:"""
---
+++ llm_solver/src/prompts/system.py
@@ -0,0 +1,2 @@
+PROMPT = """You are a software engineering expert. 
+As part of this conversation you will analyze technical questions software engineers are being asked and will suggest solutions that would be performant enough."""
